d1c364e6a519c4522822e9bbcf84e24a9efe8a94
zak-zaffar <zakzaffar@gmail.com>
2022-12-27 17:17:07 +0000

    added admin page

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -52,7 +52,7 @@ const Customers = () => {
 
     return (
     <Box m="1.5rem 2.5rem">
-        <Header title="CUSTOMERs" subtitle="list of customers" />
+        <Header title="CUSTOMERS" subtitle="list of customers" />
         <Box
             mt="40px"
             height="75vh"
